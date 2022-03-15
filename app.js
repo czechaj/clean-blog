bd2e44cf358f3aa1d342cf40b2cd1a24040b0a97
cze <cze_14@outlook.com>
2022-03-15 13:47:23 +0300

    public%views added, ejs settings done

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 
-const app = express()
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    const blog = {
-      id: 1, 
-      title: "Blog title",
-       description: "Blog description" 
-    };
-    res.send(blog);
-  });
-  
-  const port = 3000;
-  
-  app.listen(port, () => console.log(`Sunucu port ${port}'de baslatildi`));
-  
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Sunucu port ${port}'de baslatildi`));
