b61dc2a66b3e37aee6203de0f5176dc5493a5fc7
cze <cze_14@outlook.com>
2022-03-15 19:08:08 +0300

    db interaction added

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const Post = require('./models/Post');
 
 const app = express();
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -23,7 +33,10 @@ app.get('/post', (req, res) => {
   res.render('post');
 });
 
-
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
 
 const port = 3000;
 
